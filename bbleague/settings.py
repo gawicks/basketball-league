c2ae96e499db389465e676f56be0b833a19be5dd
gawicks <harithaw@different.com.au>
2022-11-04 22:30:16 +0530

    move stat middleware

--- bbleague/settings.py
+++ bbleague/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'bbleague.middleware.stat.stat_middleware',
+    'api.middleware.stat.stat_middleware',
 
 ]
 
